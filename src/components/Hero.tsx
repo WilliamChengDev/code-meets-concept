66748f3deaac9aeb8b20e89c9776a36deb30e2be
WilliamChengDev <williamcheng04930@gmail.com>
2025-06-06 20:19:49 -0500

    completed most hero section animations. Next: fix spaces in editor,move on to next page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,38 @@
 import './Hero.css';
 import { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin';
+import { TextPlugin } from "gsap/TextPlugin";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP, ScrambleTextPlugin, TextPlugin);
 
 function Hero() {
 
-        const line11Ref = useRef<HTMLDivElement>(null);
-        const line13Ref = useRef<HTMLDivElement>(null);
-        const line18Ref = useRef<HTMLDivElement>(null);
-        const subtitleRef = useRef<HTMLDivElement>(null);
-        const williamRef = useRef<HTMLDivElement>(null);
-        const chengRef = useRef<HTMLDivElement>(null);
+        const line11Ref = useRef<HTMLDivElement>(null); //for subtitle position
+        const line13Ref = useRef<HTMLDivElement>(null); //for William position
+        const line18Ref = useRef<HTMLDivElement>(null); //for Cheng position
+        const subtitleRef = useRef<HTMLDivElement>(null); //subtitle object
+        const williamRef = useRef<HTMLDivElement>(null); //william object
+        const chengRef = useRef<HTMLDivElement>(null); //cheng object
+        const editorContainerRef = useRef<HTMLDivElement>(null); //contains all lines of editor code
+        const lineRef = useRef<HTMLDivElement>(null); //for the height of a line
+
+        useGSAP(() => {
+
+                let rectangleBlink = gsap.timeline({repeat: -1}); //terminal-text-rectangle blink
+                        rectangleBlink.to(".terminal-rectangle", { duration: 2, opacity: 0, yoyo: true, ease: "power2.inOut"})
+
+                let heroIn = gsap.timeline(); //hero intro animation line
+                        heroIn.to(".william", { duration: 2, scrambleText: { text: "William", revealDelay: 0, speed: 0.5, } }, "<") //title scramble
+                        .to(".cheng", { duration: 2, scrambleText: { text: "Cheng", revealDelay: 0, speed: 0.5, } }, "<") //title scramble
+                        .to(".subtitle-text", { duration: 2, scrambleText: { text: "Code Meets Concept", revealDelay: 0, speed: 0.5, } }, "<") //subtitle scramble
+                        .from(".terminal-section-container", { duration:3, marginTop:"1000px", ease: "power2.inOut" }, "<")  //terminal window glide-in
+
+                let codeEditor = gsap.timeline(); //code editor animations
+                        codeEditor.from(".animated-line", {duration: 3, text: ""})
+
+        }) //Optional: can add scope at the end here
 
         const updateOverlay = () => {
                 if (
@@ -17,12 +41,15 @@ function Hero() {
                         !line18Ref.current ||
                         !subtitleRef.current ||
                         !williamRef.current ||
-                        !chengRef.current
+                        !chengRef.current ||
+                        !editorContainerRef.current ||
+                        !lineRef.current
                 ) {
                   return;
                 }
-                
-                const lineHeight = line13Ref.current.getBoundingClientRect().height;
+
+                const lineHeight = lineRef.current.getBoundingClientRect().height;
+                // const lineHeight = line13Ref.current.getBoundingClientRect().height;
                 
                 const line11Rect = line11Ref.current.getBoundingClientRect();
                 const line13Rect = line13Ref.current.getBoundingClientRect();
@@ -39,12 +66,7 @@ function Hero() {
                 const chengSizeBias = 1.65
                 const chengTopBias = 0.95
 
-
-                console.log("line13Bottom", line13Rect.bottom);
-                console.log("line18Top", line18Rect.top);
-                console.log("lineHeight", lineHeight);
-                console.log("williamTopPx", williamTopPx);
-                console.log("gapPx", gapPx);
+                const lines = editorContainerRef.current.querySelectorAll<HTMLDivElement>('div'); //select all child divs
             
                 // set position, font size, and line height of the "William" text
                 williamRef.current.style.top = williamTopPx * williamTopBias+ "px";
@@ -59,8 +81,18 @@ function Hero() {
                 subtitleRef.current.style.fontSize = lineHeight * 2 + "px";
                 subtitleRef.current.style.lineHeight = lineHeight * 2 + "px";
 
+                //set height of each editor container line
+                lines.forEach(line => {
+                        line.style.minHeight = `calc(${lineHeight}px - 0.2rem)`; //subtract top padding of 0.2rem
+                })
+
+                //debug
+                console.log("line13Bottom", line13Rect.bottom);
+                console.log("line18Top", line18Rect.top);
+                console.log("lineHeight", lineHeight);
+                console.log("williamTopPx", williamTopPx);
+                console.log("gapPx", gapPx);
         };
-            
 
         useLayoutEffect(() => {
                 // Run once on mount:
@@ -96,40 +128,30 @@ function Hero() {
                         <div className='code-section-container'>
                                 <div className='numbered-lines-container'>
                                         {Array.from({ length: 70 }, (_, i) => (
-                                                i < 9 ? <div key={"num" + i}>{"0" + (i + 1)}</div> : <div key={"num" + i}>{i + 1}</div>
+                                                i == 0 ? <div key={"num" + i} ref={lineRef}>{"0" + (i + 1)}</div> :
+                                                        i < 9 ? <div key={"num" + i}>{"0" + (i + 1)}</div> : <div key={"num" + i}>{i + 1}</div>
                                         ))}
                                 </div>
                                 <div className='number-editor-bar'></div>
-                                <div className='editor-container'>
-                                        <div>{"import CODE from 'CONCEPT'"}</div>
-                                        <div>{"import ReactDOM from 'react-dom/client'"}</div>
+                                <div className='editor-container' ref={editorContainerRef}>
+                                        <div className='animated-line'>{"import CODE from 'CONCEPT'"}</div>
+                                        <div className='animated-line'>{"import ReactDOM from 'react-dom/client'"}</div>
 
-                                        {/* This is the line we want to “dash‐fill” */}
-                                        <div className="code-with-effect">
-                                                <span className="code-text">import App from './App.tsx'</span>
-                                                <span>{"//"}</span>
-                                                <span className="dash-fill"></span>
-                                        </div>
+                                        <div className='animated-line'>{"import App from './App.tsx' //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}</div>
 
-                                        <div>{"import './index.css'"}</div>
+                                        <div className='animated-line'>{"import './index.css'"}</div>
                                         <div>{"\u00a0"}</div>
-                                        <div>{"ReactDOM.createRoot(document.getElementById('root')!).render("}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0<React.StrictMode>"}</div> {/* Use nbsp as space since whitespace: nowrap */}
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<App/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0</React.StrictMode>"}</div>
+                                        <div className='animated-line'>{"ReactDOM.createRoot(document.getElementById('root')!).render("}</div>
+                                        <div className='animated-line indent1'>{"<React.StrictMode>"}</div> {/* Use nbsp as space since whitespace: nowrap */}
+                                        <div className='animated-line indent2'>{"<App/>"}</div>
+                                        <div className='animated-line indent1'>{"</React.StrictMode>"}</div>
                                         <div>{")"}</div>
 
-                                        <div className="code-with-effect" ref={line11Ref}>
-                                                <span>{"//"}</span>
-                                                <span className="dash-fill"></span>
-                                        </div>
+                                        <div className='animated-line' ref={line11Ref}>{"//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}</div>
+                                        <div className='animated-line'>{"export default function App() {"}</div>
 
-                                        <div>{"export default function App() {"}</div>
+                                        <div className='animated-line' ref={line13Ref}>{"//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}</div>
 
-                                        <div className="code-with-effect" ref={line13Ref}>
-                                                <span>{"//"}</span>
-                                                <span className="dash-fill"></span>
-                                        </div>
                                         <div className="code-with-effect">
                                                 <span>{"//"}</span>
                                                 <span className='plus-seg'></span>
@@ -150,56 +172,48 @@ function Hero() {
                                                 <span className='blank-seg'></span>
                                                 <span className='star-seg'></span>
                                         </div>
-                                        <div className="code-with-effect" ref={line18Ref}>
-                                                <span>{"//"}</span>
-                                                <span className='min-seg'></span>
-                                                <span className='blank-seg'></span>
-                                                <div className='dash-fill'></div>
-                                        </div>
-                                        <div className="code-with-effect">
-                                                <span>{"//"}</span>
-                                                <span className='min-seg'></span>
-                                                <span className='min-seg'></span>
-                                        </div>
-                                        <div className="code-with-effect">
-                                                <span>{"//"}</span>
-                                                <span className='star-seg'></span>
-                                                <span className='blank-seg'></span>
-                                                <span className='plus-seg'></span>
-                                        </div>
-                                        <div className="code-with-effect">
-                                                <span>{"//"}</span>
-                                                <span className='blank-seg'></span>
-                                                <span className='min-seg'></span>
-                                                <span className='min-seg'></span>
-                                        </div>
-                                        <div className="code-with-effect">
-                                                <span>{"//"}</span>
-                                                <span className='plus-seg'></span>
-                                                <span className='plus-seg'></span>
-                                        </div>
-                                        <div className="code-with-effect">
-                                                <span>{"//"}</span>
-                                                <span className='dash-fill'></span>
-                                        </div>
-
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0return ("}</div>
                                         <div className='code-with-effect'>
-                                                <span>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0 <>\u00a0<!"}</span>
-                                                <span className='dash-fill'></span>
+                                                <div className='animated-line' ref={line18Ref}>//------------------------/</div>
+                                                <div className='animated-line'>/-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</div>
                                         </div>
+                                        <div className="code-with-effect">
+                                                <span>{"//"}</span>
+                                                <span className='min-seg'></span>
+                                                <span className='min-seg'></span>
+                                        </div>
+                                        <div className="code-with-effect">
+                                                <span>{"//"}</span>
+                                                <span className='star-seg'></span>
+                                                <span className='blank-seg'></span>
+                                                <span className='plus-seg'></span>
+                                        </div>
+                                        <div className="code-with-effect">
+                                                <span>{"//"}</span>
+                                                <span className='blank-seg'></span>
+                                                <span className='min-seg'></span>
+                                                <span className='min-seg'></span>
+                                        </div>
+                                        <div className="code-with-effect">
+                                                <span>{"//"}</span>
+                                                <span className='plus-seg'></span>
+                                                <span className='plus-seg'></span>
+                                        </div>
+                                        <div className='animated-line'>{"//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}</div>
 
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<PreLoader loaded={loaded} trigger = {startPage}/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<div className='app-container' onWheel={handleScroll}>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<TransitionCover page={scroll}/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<Loader/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<TopRow/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<Home/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0<Page1Blender/>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0</div>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0</>"}</div>
-                                        <div>{"\u00a0\u00a0\u00a0\u00a0\u00a0)"}</div>
-                                        <div>{"}"}</div>
+                                        <div className='animated-line indent1'>{"return ("}</div>
+                                        <div className='animated-line indent2'>{"<>\u00a0<!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}</div>
+
+                                        <div className='animated-line indent3'>{"<PreLoader loaded={loaded} trigger = {startPage}/>"}</div>
+                                        <div className='animated-line indent3'>{"<div className='app-container' onWheel={handleScroll}>"}</div>
+                                        <div className='animated-line indent4'>{"<TransitionCover page={scroll}/>"}</div>
+                                        <div className='animated-line indent4'>{"<Loader/>"}</div>
+                                        <div className='animated-line indent4'>{"<TopRow/>"}</div>
+                                        <div className='animated-line indent4'>{"<Home/>"}</div>
+                                        <div className='animated-line indent4'>{"<Page1Blender/>"}</div>
+                                        <div className='animated-line indent3'>{"</div>"}</div>
+                                        <div className='animated-line indent2'>{"</>"}</div>
+                                        <div className='animated-line indent1'>{")"}</div>
+                                        <div className='animated-line'>{"}"}</div>
                                 </div>
                         </div>
                         <div className='terminal-section-container'>
@@ -229,8 +243,8 @@ function Hero() {
                         <div className='subtitle-comment'>//</div>
                         <div className='subtitle-text'>Code Meets Concept</div>
                 </div>
-                <div className='william' ref={williamRef}>William</div>
-                <div className='cheng' ref={chengRef}>Cheng</div>
+                <div className='william' ref={williamRef}>FstName</div>
+                <div className='cheng' ref={chengRef}>Loading...</div>
         </div>
         );
 }
