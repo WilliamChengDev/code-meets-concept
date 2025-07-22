375f1742d01e6122c67d1f40d601e7ffb3603653
WilliamChengDev <williamcheng04930@gmail.com>
2025-07-22 18:13:50 -0500

    fixed incorrect base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/code-meets-concept/',  // Replaced with actual GitHub repo name
   plugins: [react()],
 })
